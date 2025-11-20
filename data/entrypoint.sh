3d3f16a87ec532404b539de47c5570b73142b443
pplulee <hi@pplulee.me>
2025-11-20 21:38:49 +0800

    更新地区检测方法

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -3,14 +3,17 @@ set -e
 mkdir -p /root/.cache/crontab
 CRON_JOB="* * * * * /usr/local/bin/php /var/www/html/think cronJob > /dev/null 2>&1"
 geo_check() {
-    api_list="https://cloudflare.com/cdn-cgi/trace https://blog.cloudflare.com/cdn-cgi/trace https://dash.cloudflare.com/cdn-cgi/trace https://developers.cloudflare.com/cdn-cgi/trace"
+    api_list=(
+        "https://cloudflare.com/cdn-cgi/trace"
+        "https://blog.cloudflare.com/cdn-cgi/trace"
+        "https://dash.cloudflare.com/cdn-cgi/trace"
+        "https://developers.cloudflare.com/cdn-cgi/trace"
+    )
     ua="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
     isCN="false"
-    for url in $api_list; do
-        text="$(curl -A "$ua" -m 10 -s "$url" 2>/dev/null)"
-        if [ -z "$text" ]; then
-            continue
-        fi
+    for url in "${api_list[@]}"; do
+        text="$(curl -A "$ua" -m 10 -sSL "$url" 2>/dev/null)" || continue
+        [ -z "$text" ] && continue
         location="$(echo "$text" | grep '^loc=' | cut -d'=' -f2)"
         if [ -n "$location" ]; then
             if [ "$location" = "CN" ]; then
@@ -20,7 +23,7 @@ geo_check() {
             fi
             break
         else
-            if echo "$text" | grep -q 'CN'; then
+            if echo "$text" | grep -q 'loc=CN'; then
                 isCN="true"
                 break
             fi
@@ -29,7 +32,7 @@ geo_check() {
 }
 geo_check
 cd /var/www/html || exit 1
-if [ -n "$isCN" ]; then
+if [ "$isCN" = "true" ]; then
     echo "使用国内镜像"
     composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
 fi
